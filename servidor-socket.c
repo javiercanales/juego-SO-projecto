ea4efc7ec8e0318f02af18480032bc0f726bd3a1
Javier Ignacio Canales Castro <javier.canales1601@alumnos.ubiobio.cl>
2020-03-19 01:00:10 -0300

    Varios cambios en el codigo, preparando escenario de comunicacion (solo comunicacion, no sincro)

--- servidor-socket.c
+++ servidor-socket.c
@@ -5,46 +5,71 @@
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
+#define TRUE  1
+#define FALSE 0
+#define PUERTO 8080
+#define NUM_CLIENTES 2
 
 int main() {
-	int sockfd, connfd, lenbuf;
+	int sockfd, connfd, lenbuf, i;
+	int clientes_socket[2] = {0,0};      // Lista de clientes/jugadores
 	socklen_t lencli;
 	char buf[1024];
-	struct sockaddr_in servidor; // Estructura que almacenará donde y como se establecerá la conexión
-	struct sockaddr cliente; // Estructura donde se almacena la información de conexión del cliente.
+	struct sockaddr_in servidor; // Estructura que almacenará donde y como se establecerá la conexión.
+	struct sockaddr cliente; // Estructura donde se almacena la información de conexión de los clientes.
 	
 	sockfd = socket(AF_INET, SOCK_STREAM, 0); // Se crea el socket con el protocolo TCP/IP (AF_INET), destinado a envio de mensajes (SOCK_STREAM)
 											  // El sistema operativo retorna el descriptor del socket.
-	
+
 	if(sockfd == -1) {
         return -1;
     }
     
     servidor.sin_family = AF_INET; // Se establece el protocolo de comunicación en la estructura
     servidor.sin_addr.s_addr = inet_addr("127.0.0.1"); // El servidor define la ip donde alojará el socket
-    servidor.sin_port = htons(8080); // También el puerto
+    servidor.sin_port = htons(PUERTO); // También el puerto
     
     if( bind( sockfd, (struct sockaddr*) &servidor, sizeof(servidor) ) != 0 ) { // Inicializa el socket para el uso local (eventualmente también se puede remotamente)
         return -1;
     }
     
-    if( listen(sockfd, 1) != 0 ) { // Establece cuandos usuarios se van a conectar, en este caso se establece solo uno.
+    if( listen(sockfd, NUM_CLIENTES) != 0 ) { // Establece cuantos usuarios se van a conectar, en este caso son 2 (jugadores).
         return -1;
     }
-    printf("Esperando cliente...\n");
-    connfd = accept(sockfd, &cliente, &lencli); // Espera hasta que se conecte un cliente y se almancena su información
-    
-    if(connfd < 0) {
-		return -1;
-	}
+    printf("Esperando jugadores...\n");
 	
-	printf("Cliente conectado!\nAhora se puede recibir mensajes del cliente...\n\n");
 	
-	while(1) {
+	while(TRUE) { // Se inicia el módulo para controlar conexiones y acciones del juego
+
+		for(i=0; i<NUM_CLIENTES; i++) {
+
+			connfd = accept(sockfd, &cliente, &lencli); // Espera hasta que se conecte un cliente y se almacena su información
+
+            if(connfd < 0) {
+		        return -1;
+            }
+
+            // Agrega el fd del nuevo socket a la lista de clientes/jugadores
+            if(clientes_socket[i] == 0) {   
+
+                clientes_socket[i] = connfd;
+                printf("Agregando jugador numero: %d\n" , i+1); 
+            }
+
+            // Algo con fork()...
+
+            if (i==0) { // Si aun no llega a conectar 2 jugadores
+            	printf("Jugador conectado!\nFalta 1 jugador mas para iniciar, esperando jugador...\n\n");
+            } else {
+            	printf("Jugador conectado!\nIniciando partida, cargando...\n");
+            	break;
+            }
+		}
+
 		if( (lenbuf = recv(connfd, buf, 1024, 0)) == 0 ) { // recv se encarga de recibir mensajes desde algún cliente, almacenando de una cadena de caracteres
 			printf("\nError al recibir mensaje\n");
 			return -1;
-		}else {
+		} else {
 			write(1, buf, lenbuf); // Lo escribe por pantalla
 		}
 	}
--- servidor-tuberia.c
+++ servidor-tuberia.c
@@ -1,6 +1,68 @@
+/* 
+Tuberia.c
+Tuberias con nombre: programa para el servidor.
+*/
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
 #include <stdio.h>
 
-void main(){
-	printf("Hola mundooo!");
-	return;
+#define FIFONAME "servidor-padre-hijo" // Nombre que se le va a dar al archivo al ser creado. No necesariamente definirlo de esta forma, también puede ser con un string
+#define FIFOCCNAME "servidor-hijo-cliente" // Nombre del archivo para enlace CC (Child-Client) (es un nombre de referencia solamente)
+#define NUM_CLIENTES 2
+
+int main(void){
+	int n,fd,i,fdcc; // fdcc: file descriptor child-client
+	char buf[1024]; // Cadena de char usado para guardar lo que se recibe desde el cliente.
+	unlink(FIFONAME); // Elimina "servidor-padre-hijo" si existe.
+	unlink(FIFO2NAME); // Elimina "servidor-hijo-cliente" si existe.
+
+	if(mkfifo(FIFONAME,0666)<0){ // Crea que archivo "servidor-padre-hijo" con permisos 666, si no existe lo crea.
+		perror("mkfifo"); // Si no se puede crear, imprime el error causado.
+		exit(1);
+	}
+	if((fd=open(FIFONAME,O_RDWR))<0){ // Se abre el archivo "servidor-padre-hijo" con permisos de lectura y escritura. Se guarda su descriptor de archivo (fd)
+		perror("open"); // Si falla, error
+		exit(1);
+	}
+
+	for(i=0; i<NUM_CLIENTES; i++) {
+
+    	// Acciones hijos
+		if(fork()==0){
+
+			if(mkfifo(FIFOCCNAME,0666)<0) { // Crea que archivo "servidor-hijo-cliente" con permisos 666, si no existe lo crea.
+
+				perror("mkfifo"); // Si no se puede crear, imprime el error causado.
+				exit(1);
+			}
+			if((fdcc = open(FIFOCCNAME,O_RDWR))<0){ // Se abre el archivo "servidor-hijo-cliente" con permisos de lectura y escritura. Se guarda su descriptor de archivo (fd)
+				
+				perror("open"); // Si falla, error
+				exit(1);
+			}
+			// Manejar con una variable i a cada hijo, de alguna forma
+	        while((n = read(fd,buf,sizeof(buf)))>0){ // Lee desde la tuberia los mensajes que va escribiendo el cliente, lo almacena en buf y retorna en número de bytes escritos
+		        
+		        write(1,buf,n); // Escribe por pantalla los almacenado en buf (escribe n bytes).
+		    }
+	    	//más cosas -------------
+		    close(fd);
+		}
+	}
+	
+	//acciones padre
+	else {
+		if((fd=open(FIFONAME,O_RDWR))<0){ // Se abre el archivo FIFO con permisos de lectura y escritura. Se guarda su descriptor de archivo (fd)
+		    
+		    perror("open"); // Si falla, error
+		    exit(1);
+	    }
+	    //más cosas -----------
+	}
+	
+	exit(0);
 }
