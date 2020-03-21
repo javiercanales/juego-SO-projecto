85a008b5989ea349898ada21ca9af25c8224d6fe
Javier Ignacio Canales Castro <javier.canales1601@alumnos.ubiobio.cl>
2020-03-20 22:04:45 -0300

    preparando interaccion cliente-servidor

--- cliente-tuberia.c
+++ cliente-tuberia.c
@@ -9,13 +9,13 @@ Tuberias con nombre: programa para el cliente.
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 #define FIFOC1 "myfifo-cliente-1" // Nombre del archivo para enlace cliente 1 (es un nombre de referencia únicamente)
 #define FIFOC2 "myfifo-cliente-2" // ** cliente 2
 
 int main (void){
 	int n,fd;
 	char buf[1024]; // Cadena de char usado para guardar lo que se escribe en el cliente.
-	char latido[8] = "aviso"; // Para avisar conexion
 	
 	printf("Conectando en jugador 1...\n");
 	if((fd=open(FIFOC1,O_RDWR))<0){ // Se abre el archivo FIFO con permisos de lectura y escritura. Se guarda su descriptor de archivo (fd)
@@ -27,12 +27,30 @@ int main (void){
 			
 			perror("open"); // Si falla, error
 			exit(1);
+		} else {
+			printf("Conexion lograda - Preparando...\n");
+			write(fd, "aqui-cliente", 12);
 		}
+	} else {
+		printf("Conexion lograda - Preparando...\n");
+		write(fd, "aqui-cliente", 12);
 	}
     //chmod(FIFOC1, 0000);
-    write(fd,latido,sizeof(latido));
 
-	while((n=read(0,buf,sizeof(buf)))>0){ // Lee desde teclado el mensaje que se quiere enviar al servidor.
+    while((n = read(fd,buf,sizeof(buf)))>0) { // Lee mensajes desde el servidor, lo almacena en buf y retorna en número de bytes escritos
+
+		if(buf[0] == '$') { // Si es su turno (definimos el $ como 1er caracter para indicar turno)
+			printf("Es su turno!, ingrese su eleccion --------\n")
+			// Acciones para mostrar matriz juego
+			n = read(0,buf,sizeof(buf)); // Lee desde teclado el mensaje que se quiere enviar al servidor.
+            write(fd,buf,n); // Si se recibe más de 0 bytes, se escribe en la tuberia (fd) lo almacenado en buf con n bytes
+		} else {
+
+		}
+
+		write(1,buf,n); // Escribe por pantalla lo almacenado en buf (escribe n bytes).
+	}
+	while((n = read(0,buf,sizeof(buf)))>0) { // Lee desde teclado el mensaje que se quiere enviar al servidor.
 		write(fd,buf,n); // Si se recibe más de 0 bytes, se escribe en la tuberia (fd) lo almacenado en buf con n bytes
 	}
 	close(fd);
--- servidor-tuberia.c
+++ servidor-tuberia.c
@@ -9,7 +9,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <stdio.h>
-
+#include <string.h>
 /**
 * En este sistema se crean 3 tuberias, la primera, creada por el padre (con mkfifo(FIFONAME,0666))
 * La segunda y tercera, creadas por
@@ -23,16 +23,17 @@
 int main(void){
 	int n, fd, fd1, fd2, i;
 	char buf[1024]; // Cadena de char usado para guardar lo que se recibe desde el cliente.
+
 	unlink(FIFONAME); // Elimina "my-fifo" si existe.
 	unlink(FIFOC1); // Elimina "myfifo-cliente-1" si existe.
 	unlink(FIFOC2); // Elimina "myfifo-cliente-2" si existe.
 
 
-	if(mkfifo(FIFONAME,0666)<0){ // Crea archivo "servidor-padre-hijo" con permisos 666, si no existe lo crea.
+	if(mkfifo(FIFONAME,0666)<0){ // Crea archivo "myfifo" con permisos 666, si no existe lo crea.
 		perror("mkfifo"); // Si no se puede crear, imprime el error causado.
 		exit(1);
 	}
-	if((fd = open(FIFONAME,O_RDWR))<0){ // Se abre el archivo "servidor-padre-hijo" con permisos de lectura y escritura. Se guarda su descriptor de archivo (fd)
+	if((fd = open(FIFONAME,O_RDWR))<0){ // Se abre el archivo "myfifo" con permisos de lectura y escritura. Se guarda su descriptor de archivo (fd)
 		perror("open"); // Si falla, error
 		exit(1);
 	}
@@ -40,7 +41,7 @@ int main(void){
 	//Bloquear a cliente conectado mientras se espera
 	//n = read(fd,buf,sizeof(buf)); //aun no leeremos de aqui, testeando clientes 1ro
 
-	printf("Iniciando servidor...");
+	printf("Iniciando servidor...\n");
 	for(i=1; i <= NUM_CLIENTES; i++) {
 
     	// Acciones hijos
@@ -60,9 +61,16 @@ int main(void){
 		                exit(1);
 	                }
 
-				    n = read(fd1,buf,sizeof(buf)); // Se lee para detectar una conexion entrante (cliente)
-				    chmod(FIFOC1, 0000); // Vez detectada conexion, se cambia permiso asignado a la tuberia, para que no entren mas clientes.
-				    printf("Jugador 1 conectado!\n");
+				    n = read(fd1,buf,sizeof(buf)); // Se lee para detectar una conexion entrante (cliente) (un latido)
+                    if(strcmp ("aqui-cliente", buf) == 0) { // Se evalua que el latido sea correcto 
+                    	                                   // (podria requerir cambios este metodo, o mejoras, ir viendo)
+				    	printf("Jugador 1 conectado!\n");
+				    } else {
+				    	exit(1);
+				    }
+
+				    chmod(FIFOC1, 0000); // Una vez detectada conexion, se cambia permiso asignado a la tuberia, para que no entren mas clientes.
+				    
 				    // Quiza sirva hacer el chmod 0000 en el cliente, habra que probar (seria mas eficiente para evitar errores)
 				    
 		            while((n = read(fd1,buf,sizeof(buf)))>0){ // Lee desde la tuberia los mensajes que va escribiendo el cliente, lo almacena en buf y retorna en número de bytes escritos
@@ -70,8 +78,6 @@ int main(void){
 		                write(1,buf,n); // Escribe por pantalla los almacenado en buf (escribe n bytes).
 		                // Acciones
 		            }
-	    	        // Más cosas -------------
-		            close(fd1); // Va?? verificar
 
 				    break;
 
@@ -87,15 +93,19 @@ int main(void){
 	                }
 
 				    n = read(fd2,buf,sizeof(buf)); // Se lee para detectar conexion entrante de cliente 2 (ocurrirá sólo cuando exista conexión en tuberia 1)
-				    printf("Jugador 2 conectado!\n");
+				    if(strcmp ("aqui-cliente", buf) == 0) {
+				    	printf("Jugador 2 conectado!\n");
+				    } else {
+				    	exit(1);
+				    }
+
+                    write(fd, "init-game", 9); // Se avisa al padre que los 2 clientes están conectados (ocurre sólo en tal caso para este sistema)
 
                     while((n = read(fd2,buf,sizeof(buf)))>0){ // Lee desde la tuberia los mensajes que va escribiendo el cliente, lo almacena en buf y retorna en número de bytes escritos
 		        
 		                write(1,buf,n); // Escribe por pantalla los almacenado en buf (escribe n bytes).
 		                // Acciones
 		            }
-	    	        // Más cosas -------------
-		            close(fd2); // Va?? verificar
 
 				    break;
 
@@ -105,16 +115,18 @@ int main(void){
 		}
 
 	}
-	
-	//acciones padre (no deberia ningun hijo llegar aquí)
-	if((fd=open(FIFONAME,O_RDWR))<0){ // Se abre el archivo FIFO con permisos de lectura y escritura. Se guarda su descriptor de archivo (fd)
-		    
-		perror("open"); // Si falla, error
-		exit(1);
+
+	// Acciones padre (Control general partida del servidor) --- no se requiere derivar de un "else" del "if (fork() == 0)"
+	// (como hemos hecho en ejemplos) pues los hijos nunca llegarán aquí (por diseño general del código)
+
+	n = read(fd,buf,sizeof(buf));
+	while(strcmp ("init-game", buf) != 0) { // Servidor-padre espera confirmación de conexión jugador 2, logrado esto, comienza.
+		n = read(fd,buf,sizeof(buf));
 	}
+	printf("Iniciando control de partida 1v1 --------\n");
 
 	//más cosas -----------
-	while((n=read(fd,buf,sizeof(buf)))>0){ // Lee desde la tuberia los mensajes que va escribiendo el cliente, lo almacena en buf y retorna en número de bytes escritos
+	while((n = read(fd,buf,sizeof(buf)))>0){ // Lee desde la tuberia los mensajes que va escribiendo el cliente, lo almacena en buf y retorna en número de bytes escritos
 		write(1,buf,n); // Escribe por pantalla los almacenado en buf (escribe n bytes).
 	}
 
